111ef69699b990f5ab640f857e6333315a293ac9
AbrahamNeme <128229923+AbrahamNeme@users.noreply.github.com>
2024-01-15 21:48:54 +0100

    Update urls.py

--- EChemServer/urls.py
+++ EChemServer/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import get_all_cventry, get_cventry_by_name, get_cventry_by_material, get_normalized_cventry
 
 urlpatterns = [
-    path('all/', get_all_cventry),
-    path('entrybyname/<str:name>/', get_cventry_by_name),
-    path('entrybymaterial/<str:material>/', get_cventry_by_material),
+    path('all/', views.get_all_cventry, name='get_all_cventry'),
+    path('entrybymaterial/<str:material>/', views.get_cventry_by_material, name='get_cventry_by_material'),
+    path('entrybyname/<str:name>/', views.get_cventry_by_name, name='get_cventry_by_name'),
+    path('entriesbyname/<str:names>/', views.get_cventries_by_name, name='get_cventries_by_name'),
 ]

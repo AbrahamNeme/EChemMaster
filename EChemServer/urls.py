79af0a17f8dbc18cb1776eaded726d89e697cf33
AbrahamNeme <128229923+AbrahamNeme@users.noreply.github.com>
2024-01-15 21:46:50 +0100

    Update urls.py

--- EChemServer/urls.py
+++ EChemServer/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('all/', get_all_cventry),
     path('entrybyname/<str:name>/', get_cventry_by_name),
     path('entrybymaterial/<str:material>/', get_cventry_by_material),
-    path('get_normalized_cventry/<str:name>/', get_normalized_cventry, name='get_normalized_cventry'),
 ]
